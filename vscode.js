94c4c677d6c990144be8ff6a23ed7b404c2420df
Juan Camilo Quintero <juancamiloqhz@gmail.com>
2018-10-12 11:52:28 -0500

    Update .vscode
    Start my own way

--- vscode.js
+++ vscode.js
@@ -1,63 +1,54 @@
 {
-  "window.zoomLevel": -1,
-  "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
-  "editor.fontSize": 29,
-  "editor.lineHeight": 40,
+  "editor.fontSize": 16,
   "editor.letterSpacing": 0.5,
-  "workbench.editor.tabSizing": "shrink",
+  "editor.lineHeight": 22,
+  "editor.tabSize": 2,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true
+    //"**/.next": true
+  },
+  "files.trimTrailingWhitespace": true,
+  "gitlens.advanced.messages": {
+    "suppressShowKeyBindingsNotice": true
+  },
   "html.format.enable": true,
   "html.format.preserveNewLines": true,
-  "files.trimTrailingWhitespace": true,
-  "editor.tabSize": 2,
-  // this isn't really underline but we hack it to be a thicker cursor
+  "terminal.integrated.fontSize": 14,
+  "window.zoomLevel": 1,
+  "workbench.colorTheme": "Cobalt2",
+  "workbench.editor.tabSizing": "shrink",
+  "workbench.iconTheme": "vscode-icons",
   "editor.cursorStyle": "line",
-  "editor.cursorWidth": 3, 
+  "editor.cursorWidth": 3,
   "editor.wordWrap": "on",
   "editor.multiCursorModifier": "ctrlCmd",
   "editor.fontWeight": "400",
   "editor.cursorBlinking": "solid",
   "editor.colorDecorators": false,
   "editor.minimap.enabled": false,
-  "vscode_custom_css.imports": [
-    "/Volumes/Macintosh HD/Users/wesbos/.vscodestyles.css"
-  ],
   "workbench.activityBar.visible": true,
   "workbench.sideBar.location": "left",
-  "gitlens.blame.line.enabled": false,
   "editor.renderWhitespace": "none",
-  "editor.rulers": [
-    80,
-    100,
-    120
-  ],
+  "editor.rulers": [80, 100, 120],
   "editor.minimap.showSlider": "always",
   "workbench.startupEditor": "newUntitledFile",
   "extensions.ignoreRecommendations": false,
-  "files.exclude": {
-    "**/.git": true,
-    "**/.svn": true,
-    "**/.hg": true,
-    "**/CVS": true,
-    "**/.DS_Store": true,
-    "**/.next": true
-  },
-  "workbench.colorTheme": "Cobalt2",
   "workbench.editor.tabCloseButton": "off",
-  "emmet.includeLanguages": {
-    "javascript": "javascriptreact"
-  },
-  // show snippets before any other auto completion
   "editor.snippetSuggestions": "top",
-  // this lets you quickly open multiple items in a row and not have the previous ones closed on you
   "workbench.editor.enablePreviewFromQuickOpen": false,
   "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
-  "sublimeTextKeymap.promptV3Features": true,
-  // These are all my auto-save configs
   "editor.formatOnSave": true,
-  // turn it off for JS
   "[javascript]": {
     "editor.formatOnSave": false
   },
   "eslint.autoFixOnSave": true,
   "eslint.alwaysShowStatus": true,
+  "editor.fontFamily": "Hack, Monaco, 'Courier New', monospace"
 }
